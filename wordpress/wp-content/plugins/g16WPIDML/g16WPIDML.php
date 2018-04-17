5a89a7d418fcc17b9b84e5ca2adeef8e86c35b9b
Ghifari160 <ghifari160@ghifari160.com>
2018-04-16 19:55:03 -0500

    Feedback revisions
    There are some things that needed to be changed based on the Wordpress feedback email.

--- wordpress/wp-content/plugins/g16WPIDML/g16WPIDML.php
+++ wordpress/wp-content/plugins/g16WPIDML/g16WPIDML.php
@@ -16,14 +16,29 @@ License URI: https://github.com/Ghifari160/g16WPIDML/blob/master/LICENSE.md
 // Released under the MIT License
 // https://github.com/ghifari160/g16WPIDML/LICENSE.md
 
+// Should be specific to this plugin
 define("G16_WPIDML_DEBUG", false);
-
-define("WP_DEBUG", G16_WPIDML_DEBUG);
+define("G16_WPIDML_VER", "v0.1.0.0");
 
 if(G16_WPIDML_DEBUG)
 {
-  define("WP_DEBUG_LOG", true);
-  define("WP_DEBUG_DISPLAY", true);
+  // Set other debug stuff specific to g16WPIDML here.
+
+  // Wordpress definitions
+  if(!defined("WP_DEBUG"))
+  {
+    define("WP_DEBUG", true);
+  }
+
+  if(!defined("WP_DEBUG_LOG"))
+  {
+    define("WP_DEBUG_LOG", true);
+  }
+
+  if(!defined("WP_DEBUG_DISPLAY"))
+  {
+    define("WP_DEBUG_DISPLAY", true);
+  }
 }
 
 
@@ -35,24 +50,29 @@ function g16_wpidml_meta_box()
       ."</div>"
       ."<input type=\"file\" name=\"g16_wpidml_idml_upload\" "
       ."id=\"g16_wpidml_idml_upload\">"
-      ."<script src=\"/wp-content/plugins/g16WPIDML/WebContent/zip.js\"></script>"
       ."<script>"
-      ."zip.workerScriptsPath = \"/wp-content/plugins/g16WPIDML/WebContent/\";"
+      ."zip.workerScriptsPath = \"".plugins_url("WebContent/", __FILE__)."\";"
       ."</script>";
-
-  if(!G16_WPIDML_DEBUG)
-  {
-    echo "<script src=\"/wp-content/plugins/g16WPIDML/g16WPIDML.min.js\"></script>";
-  }
-  else
-  {
-    echo "<script src=\"/wp-content/plugins/g16WPIDML/g16WPIDML.dev.js\"></script>";
-  }
 }
 
 // Setups all meta boxes
 function g16_wpidml_init()
 {
+  wp_enqueue_script("zip.js", plugins_url("WebContent/zip.js", __FILE__));
+
+  if(!G16_WPIDML_DEBUG)
+  {
+    wp_enqueue_script("g16WPIDML.min.js",
+      plugins_url("g16WPIDML.min.js", __FILE__),
+      array("zip.js"), G16_WPIDML_VER, true);
+  }
+  else
+  {
+    wp_enqueue_script("g16WPIDML.dev.js",
+      plugins_url("g16WPIDML.dev.js", __FILE__),
+      array("zip.js"), G16_WPIDML_VER, true);
+  }
+
   add_meta_box("g16_wpidml_meta_box", "IDML => WP", "g16_wpidml_meta_box",
       "post", "normal", "high");
 }
